a34613a919c81f780d7962d671e93c96a0290b39
Evandro Carreira <evandro.carreira@gmail.com>
2014-11-12 19:08:57 -0500

    FIRST

---
+++ README.md
@@ -0,0 +1,5 @@
+# EasyQuiz
+
+Recebe um JSON com perguntas e respostas
+
+Segue as perguntas em sequência e grava as respostas
---
+++ inc/bd.php
@@ -0,0 +1,27 @@
+<?php
+class Conexao {
+	var $host = "localhost";
+	var $usuario = "root";
+	var $senha = "";
+	var $banco = "420quiz";
+
+	public $mysqli;
+	
+	public function Abrir()	{
+	$this->mysqli = new mysqli($this->host, $this->usuario, $this->senha, $this->banco);
+	}
+
+	public function Fechar() {
+		$this->mysqli->close();
+	}
+}
+class Comando {
+	public function Executar($sql) {
+		$con = new Conexao();
+		$con->Abrir(); 
+		$re = $con->mysqli->query($sql);
+		$con->Fechar();
+		return $re;
+	}
+}
+?>
