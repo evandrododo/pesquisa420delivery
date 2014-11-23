37206c6e2106381b1fcd90dd0f1d478a188f36f6
Evandro Carreira <evandro.carreira@gmail.com>
2014-11-23 20:57:17 -0200

    server name

--- facebook_login.php
+++ facebook_login.php
@@ -1,33 +1,43 @@
 <?php
-require "vendor/facebook/php-sdk-v4/autoload.php";
-
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
-
 // start session
 session_start();
+
+require "vendor/facebook/php-sdk-v4/autoload.php";
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+
 $session = FacebookSession::setDefaultApplication('225836800874912', '3789aa92507bc07715212a3e6070765a');
 
-$helper = new FacebookRedirectLoginHelper('http://'.$_SERVER['SERVER_NAME'].'/facebook_login.php?submitted=1 ');
-$loginUrl = $helper->getLoginUrl();
-// Use the login url on a link or button to redirect to Facebook for a
+$helper = new FacebookRedirectLoginHelper('http://'.$_SERVER['SERVER_NAME'].'/dev/420/pesquisa/pesquisa420delivery/facebook_login.php');
 
-if($_GET['submitted'])
-{
-    $helper = new FacebookRedirectLoginHelper();
-    try {
-      $session = $helper->getSessionFromRedirect();
-    } catch(FacebookRequestException $ex) {
-      // When Facebook returns an error
-    } catch(\Exception $ex) {
-      // When validation fails or other local issues
-    }
-    if ($session) {
-      // Logged in
-    }
+try {
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
+}
+ 
+// see if we have a session
+if ( isset( $session ) ) {
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
+   
+  // print data
+  echo  print_r( $graphObject, 1 );
+  
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl() . '">df</a>';
 }
 ?>
-<a href="<?=$loginUrl?>">Login</a>
